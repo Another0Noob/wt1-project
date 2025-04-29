8eaace1f56644a93a5cac021c198869aa67df4f7
Another0Noob <25Antonym02+github@gmail.com>
2025-04-29 22:08:05 +0200

    added Static HTML Pages

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const server = express();
 const port = 3001;
 
+// Serve static files from the 'public' directory
+server.use(express.static('public_html'));
+
 server.get("/", function (req, res) {
     res.send('<p>Welcome to the IMI server! Visit <a href="/hello">/hello</a> for a greeting.</p>');
 });
