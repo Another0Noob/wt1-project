ba8a24a1d1dad2032b9f5676517e561014932fc0
maile000 <mokona.mai@web.de>
2025-05-27 14:23:02 +0200

    json produkte, templates

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const express = require("express");
+const exphbs = require("express-handlebars"); 
+const path = require("path");
+const produkte = require("./data/produkt.json");
+
 const server = express();
 const port = 3001;
 
-// Serve static files from the 'public' directory
+server.engine("hbs", exphbs.engine({
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "templates", "layouts"),
+    partialsDir: path.join(__dirname, "templates", "partials"),
+    defaultLayout: "main",
+     helpers: {
+         stars: (anzahl) => {
+         const count = typeof anzahl === "number" && anzahl > 0 ? anzahl : 0;
+         return Array.from({ length: count });
+        }
+        }
+}));
+
+server.set("view engine", "hbs");
+
+server.set("views", path.join(__dirname, "templates"));
+
 server.use(express.static('public_html'));
 
 server.get("/", function (req, res) {
-    res.send('<p>Welcome to the IMI server! Visit <a href="/hello">/hello</a> for a greeting.</p>');
+    res.send('<p>Welcome to the IMI server! Visit <a href="/produkte">/produkte</a> to see the product list.</p>');
 });
 
 server.get("/hello", function (req, res) {
     res.send("Hello IMIs!");
 });
 
+server.get("/produkte", function (req, res) {
+    res.render("product", {
+        title: "Produktliste",
+        produkte
+    });
+});
+
+
 server.listen(port, function () {
-    console.log("Express listening on " + port);
+    console.log("Express listening on http://localhost:" + port);
 });
